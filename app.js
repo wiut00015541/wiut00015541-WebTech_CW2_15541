0653f6ad54e12bd83511e63796e4bff0b205d4a7
00015541 <wiut00015541@gmail.com>
2024-03-09 12:30:14 -1200

    datetimepicker css/js were added into head.pug . app.js and index.pug updated controllers added

--- app.js
+++ app.js
@@ -7,17 +7,22 @@ const body_parser = require('body-parser')
 // for File IO
 const path = require('path')
 
+// for web routing
+const web_route = require('./routes/web')
+
 // make mock database (raw .json file) available globally in app
 global.mock_db = path.join(__dirname, './data/mock_db.json');
 
 const app = express();
 
-app.get('/', function(req, res){
-    return res.json({
-        message: 'hello world'
-    })
+// Set the view engine for web routes
+app.set('view engine', 'pug');
 
-})
+app.use('/css', express.static('public/css'))
+app.use('/js', express.static('public/js'))
+
+app.use('/', web_route); // web routes
 
 const port = 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
