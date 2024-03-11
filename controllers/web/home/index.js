0ce5cecbdb0c00bd292a7a38ad79995fd821d932
00015541 <wiut00015541@gmail.com>
2024-03-10 17:42:45 -1200

    add/update functions were sdded

--- controllers/web/home/index.js
+++ controllers/web/home/index.js
@@ -1,7 +1,14 @@
 const home_controller = {
     index: async (req, res) =>{
         res.render('home');
-    }  
+    },
+    add: async (req, res) =>{
+        res.render('home/add_update');
+    },
+    update: async (req, res) =>{
+        res.render('home/add_update');
+    }
 };
   
 module.exports = home_controller;
+
--- routes/web/home/index.js
+++ routes/web/home/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const home_controller = require('../../../controllers/web/home');
 
 router.get('/', home_controller.index);
+router.get('/add', home_controller.add);
+router.get('/update', home_controller.update);
 
 module.exports = router;
