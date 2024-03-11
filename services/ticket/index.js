5809d418260fdec323cd2b9d46812bfb8fccfe69
00015541 <wiut00015541@gmail.com>
2024-03-10 22:38:27 -1200

    delete function added

--- services/ticket/index.js
+++ services/ticket/index.js
@@ -1,16 +1,60 @@
-// import specific service class
-const ticket_service = require('../../../services/ticket')
+const fs = require('fs')
 
-// mention the service's needed actions (methods)
-const ticket_controller = {
-    getAll(req, res) {
-        res.json(ticket_service.getAll())
+// access global mock db file
+const tickets = require(global.mock_db)
+
+// write service method implementations
+const ticket_service = {
+    getAll() {
+        return tickets
     },
+    getById(id) {
+        return tickets.find(t => t.id == id)
+    },    
     create(req, res) {
-        res.status(201).json(
-            ticket_service.create(req, res)
-        )
+        let new_id = genRandId(4)
+                
+        const ticket = req.body
+
+        const new_ticket = {
+            id: new_id,
+            ticket: ticket
+        }
+
+        tickets.push(new_ticket)
+        
+        writeToFile(tickets)
+        
+        return new_ticket
+    },
+    delete(id) {
+        const index = tickets.findIndex(u => u.id == id)
+        tickets.splice(index, 1)    
+        writeToFile(tickets)
     }
 }
 
-module.exports = ticket_controller
+// create function for overwriting the db file updated db content
+let writeToFile = async (users) => {
+    await 
+        fs.writeFileSync(
+            global.mock_db,
+            JSON.stringify(
+                users, null, 4
+            ),
+            'utf8'
+        )
+}
+
+// generate random id inspired by uuid
+let genRandId = (count) =>{
+    let result = ''
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = characters.length
+    for (let i = 0; i < count; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+}
+
+module.exports = ticket_service
