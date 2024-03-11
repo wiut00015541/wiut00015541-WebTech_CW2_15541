25b8c829cec3996e53419e60966e5c3bc10b7590
00015541 <wiut00015541@gmail.com>
2024-03-10 18:00:30 -1200

    create function added

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const body_parser = require('body-parser')
 // for File IO
 const path = require('path')
 
-// for web routing
-const web_route = require('./routes/web')
-
 // make mock database (raw .json file) available globally in app
 global.mock_db = path.join(__dirname, './data/mock_db.json');
+
+const web_route = require('./routes/web')
+const api_route = require('./routes/api');
 
 const app = express();
 
@@ -21,8 +21,16 @@ app.set('view engine', 'pug');
 app.use('/css', express.static('public/css'))
 app.use('/js', express.static('public/js'))
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', api_route); // API routes
 app.use('/', web_route); // web routes
+
+// redirect to home page if unknown requests requested
+app.use((req, res) => {
+    res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
